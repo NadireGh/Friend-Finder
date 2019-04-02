a56b1509c78c6a8fcbd89797486ff711e759a347
Nadire Ghalip <nadireghalip@Nadires-MacBook-Pro.local>
2019-04-01 23:56:17 -0700

    js files added

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendList = [
+    {
+      name: 'Nadire Ghalip',
+      profilePic: 'https://www.pexels.com/photo/nature-red-girl-model-36483/',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
+  
